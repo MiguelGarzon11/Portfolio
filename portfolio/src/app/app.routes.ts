10934f376f447496c60d389748dc21e43f18a895
Miguel Angel <miguelangelgarb@gmail.com>
2025-07-19 15:42:56 -0500

    Portfolio

--- portfolio/src/app/app.routes.ts
+++ portfolio/src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { Home } from './components/home/home';
+import { Email } from './components/email/email';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: Home },
+    { path: 'email', component: Email },
+    { path: '**', redirectTo: '' }
+];
---
+++ portfolio/tailwind.config.js
@@ -0,0 +1,18 @@
+// tailwind.config.js (Tailwind v4)
+export default {
+  content: [
+    "./src/**/*.{html,ts}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        footer: "#261423",
+        main: "#BF9E75",
+        shadow: "#D9BB96",
+        buttonhover: "#594534",
+        button: "#8C6E54",
+      },
+    },
+  },
+  plugins: [],
+};
