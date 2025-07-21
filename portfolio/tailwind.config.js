24dee3c3ba34688c06742ea50bdcf468e3236f9e
Miguel Angel <miguelangelgarb@gmail.com>
2025-07-21 17:20:27 -0500

    Cambios

--- portfolio/tailwind.config.js
+++ portfolio/tailwind.config.js
@@ -4,15 +4,7 @@ export default {
     "./src/**/*.{html,ts}",
   ],
   theme: {
-    extend: {
-      colors: {
-        footer: "#261423",
-        main: "#BF9E75",
-        shadow: "#D9BB96",
-        buttonhover: "#594534",
-        button: "#8C6E54",
-      },
-    },
+    extend: {},
   },
   plugins: [],
 };
