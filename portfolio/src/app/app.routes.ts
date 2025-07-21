6390254fe70f46b7bf72a1a8d9adc072cd9cc656
Miguel Angel <miguelangelgarb@gmail.com>
2025-07-21 17:30:15 -0500

    Reconstruccion

--- portfolio/src/app/app.routes.ts
+++
@@ -1,9 +0,0 @@
-import { Routes } from '@angular/router';
-import { Home } from './components/home/home';
-import { Email } from './components/email/email';
-
-export const routes: Routes = [
-    { path: '', component: Home },
-    { path: 'email', component: Email },
-    { path: '**', redirectTo: '' }
-];
