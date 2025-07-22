d189a384379518ebe724ff2fbcbe82d63a24b927
Miguel Angel <miguelangelgarb@gmail.com>
2025-07-22 08:50:15 -0500

    Portfolio V 2.0

--- portfolio/src/app/components/header/header.ts
+++ portfolio/src/app/components/header/header.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component } from '@angular/core';
 
 
 @Component({
@@ -6,6 +6,26 @@ import { Component } from '@angular/core';
   templateUrl: './header.html',
   styleUrl: './header.css'
 })
-export class Header {
+export class Header implements AfterViewInit {
 
+  ngAfterViewInit() {
+    const switchInput = document.querySelector<HTMLInputElement>('[data-hs-theme-switch]');
+
+    if (switchInput) {
+      switchInput.checked = document.documentElement.classList.contains('dark');
+
+      switchInput.addEventListener('change', () => {
+        const html = document.documentElement;
+        const isDark = html.classList.contains('dark');
+
+        if (isDark) {
+          html.classList.remove('dark');
+          localStorage.setItem('hs_theme', 'light');
+        } else {
+          html.classList.add('dark');
+          localStorage.setItem('hs_theme', 'dark');
+        }
+      });
+    }
+  }
 }
