e84f496316fe232ceccf83e56ae3b21fdbc6080a
Miguel Angel <miguelangelgarb@gmail.com>
2025-07-21 19:06:58 -0500

    Portfolio V.2

---
+++ portfolio/tailwind.config.js
@@ -0,0 +1,14 @@
+export default {
+  content: ["./src/**/*.{html,ts}"],
+  theme: {
+    extend: {
+      colors: {
+        main: "#222326",
+        secondary: "#d7d7d9",
+        button: "#9fa3a6",
+        buttonS: "#3c3d40",
+      },
+    },
+  },
+  plugins: [],
+};
