639c70b39442973624d5d0f57fb06c9b996de480
Miguel Angel <miguelangelgarb@gmail.com>
2025-07-25 16:11:54 -0500

    Portfolio V 2.0

--- portfolio/src/app/components/home/home.ts
+++ portfolio/src/app/components/home/home.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-home',
@@ -7,5 +7,5 @@ import { Component } from '@angular/core';
   styleUrl: './home.css'
 })
 export class Home {
-
 }
+
